1f6e8e950a24c079675a81b7343b8f37dccde14c
y o <yosukay01@gmail.com>
2020-08-24 03:47:07 -0400

    put request

--- ClientApp/src/app/features/products/product-edit/product-edit.component.ts
+++ ClientApp/src/app/features/products/product-edit/product-edit.component.ts
@@ -1,10 +1,11 @@
-import { ProductService } from './../../../data/product/product.service';
-import { map, switchMap } from 'rxjs/operators';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { ActivatedRoute, Router } from '@angular/router';
-import { Product } from 'src/app/data/product/product.model';
 import { Observable } from 'rxjs';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { map, shareReplay, switchMap } from 'rxjs/operators';
+import { Product } from './../../../data/product/product.model';
+import { ProductService } from './../../../data/product/product.service';
 
 @Component({
   selector: 'app-product-edit',
@@ -20,7 +21,8 @@ export class ProductEditComponent implements OnInit {
     private route: ActivatedRoute,
     private router: Router,
     private formBuilder: FormBuilder,
-    private productService: ProductService
+    private productService: ProductService,
+    private snackBar: MatSnackBar
   ) {
     this.form = this.formBuilder.group({
       productName: ['', Validators.required],
@@ -39,7 +41,8 @@ export class ProductEditComponent implements OnInit {
       switchMap((id) => {
         this.productId = id;
         return this.productService.getById(id);
-      })
+      }),
+      shareReplay(1)
     );
 
     this.product$.subscribe((product) => this.form.patchValue(product));
@@ -48,6 +51,11 @@ export class ProductEditComponent implements OnInit {
   onSubmit(): void {
     if (this.form.valid) {
       console.log('on submit', this.form.value);
+      const product: Product = this.form.value;
+      product.productId = this.productId;
+      this.productService
+        .put(product)
+        .subscribe((v) => this.snackBar.open('Saved'));
     }
   }
 }
